8becc64adf00b53bc57755fe43d99dc5745f22d1
이영준 <dldudwns0614@daum.net>
2023-09-25 17:56:12 +0900

    week05 v0 .2

--- go/week05/main.go
+++ go/week05/main.go
@@ -4,31 +4,11 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"log"
-	"os"
-	"strconv"
-	"strings"
+	"math/rand"
 )
 
 func main() {
-	fmt.Print("Input score : ")
-	reader := bufio.NewReader(os.Stdin)
-	inputScoreString, err := reader.ReadString('\n')
-	if err != nil {
-		log.Fatal(err)
-	}
-	inputScoreString = strings.TrimSpace(inputScoreString)      //remove space
-	inputScore, err := strconv.ParseFloat(inputScoreString, 32) //string to 32bit float
-	fmt.Println(inputScore)
-	var grade string
-	if inputScore >= 90 {
-		grade := "A grade!"
-		// fmt.Println("you got", grade)
-	} else {
-		grade = "under A grade..."
-		//fmt.Println("you got", grade)
-	}
-	fmt.Println("you got", grade)
+	dice := rand.Intn(6) + 1
+	fmt.Println(dice)
 }
