25822dfc0e965056b956e836d4ee301a286cce61
이영준 <dldudwns0614@daum.net>
2023-09-25 17:11:53 +0900

    week05 v3

--- go/week05/main.go
+++ go/week05/main.go
@@ -4,15 +4,14 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"time"
+	"os"
 )
 
 func main() {
-	var now time.Time //노란색 밑줄 상관 x
-	now = time.Now()
-	//var year int = now.Year()
-	var year = now.Year()
-	month := now.Month()
-	fmt.Println(year, month, now.Hour(), now.Minute(), now.Second())
+	fmt.Print("Input score : ")
+	reader := bufio.NewReader(os.Stdin)
+	inputScore := reader.ReadString('\n') // 1 variable but reader.ReadString returns 2 values
+	fmt.Println(inputScore)
 }
