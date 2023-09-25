bfdea7eb83947457c31124d9c2b8f912fc70898a
이영준 <dldudwns0614@daum.net>
2023-09-25 17:34:02 +0900

    week05 v0.8

--- go/week05/main.go
+++ go/week05/main.go
@@ -6,12 +6,16 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 )
 
 func main() {
 	fmt.Print("Input score : ")
 	reader := bufio.NewReader(os.Stdin)
-	inputScore := reader.ReadString('\n') // 1 variable but reader.ReadString returns 2 values
+	inputScore, err := reader.ReadString('\n') //otion 2
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println(inputScore)
 }
