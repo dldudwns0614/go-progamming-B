ed4542160d2fdc1b923ce65dde25ccaf3970d064
이영준 <dldudwns0614@daum.net>
2023-09-25 18:12:33 +0900

    week05 v0 .10

--- go/week05/main.go
+++ go/week05/main.go
@@ -5,11 +5,37 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"math/rand"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	fmt.Println("Guess number game!")
 	answer := rand.Intn(100) + 1 //1~100
 	fmt.Println(answer)
+	for i := 0; 1 < 10; i++ {
+	fmt.Print("Input number : ")
+	reader := bufio.NewReader(os.Stdin)
+	inputNumberString, err := reader.ReaderString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+	inputNumberString = strings.TrimSpace(inputNumberString)
+	inputGuess, err := strconv.Atoi(inputNumberString)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if inputGuess == answer{
+		fmt.Println("great")
+		break
+	}
+	else if inputGuess < answer {
+		fmt.Println("lower")
+	}
+	else if inputGuess > answer{
+		fmt.Println("higher")
+	}
+}
 }
