2921c371c41430126f09dfd29bfd12300a0dab85
이영준 <dldudwns0614@daum.net>
2023-10-23 17:50:41 +0900

    week09 v3

--- go/week09/main.go
+++ go/week09/main.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func swap(n1 *int, n2 *int) {
+func swap(n1 *int, n2 *int) { //두 수를 교환
 	temp := *n1
 	*n1 = *n2
 	*n2 = temp
@@ -11,7 +11,11 @@ func main() {
 	a := 1
 	b := 2
 	c := &a // var c *int = &a
-	fmt.Printf("%d %d %d\n", a, b, *c)
+	// fmt.Printf("%d %d %d\n", a, b, *c)
 	// fmt.Printf("%d %d %x\n", a, b, c)
+	fmt.Printf("%T\n", *c)
+	fmt.Println(a, b)
+	swap(&a, &b)
+	fmt.Println(a, b)
 
 }
